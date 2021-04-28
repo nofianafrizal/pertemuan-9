7fe53e2fc75185d7faff3fc37b7dd97e8ff370bb
nofianafrizal <83351171+nofianafrizal@users.noreply.github.com>
2021-04-28 23:24:53 +0700

    Add files via upload

---
+++ Pert9_latihan4.php
@@ -0,0 +1,19 @@
+<HTML>
+<HEAD>
+<TITLE> Tanggal </TITLE>
+</HEAD>
+<BODY>
+<font size="10px">
+<?php
+
+// Nama : feri
+// Kelas : 06TPLE012
+
+echo "Sekarang tanggal ";
+echo date('d-F-Y');
+echo "<br>dan jam ";
+echo date('h:i:s A');
+?>
+</FONT>
+</BODY>
+</HTML> 
